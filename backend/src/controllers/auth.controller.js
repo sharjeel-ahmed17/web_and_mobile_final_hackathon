fc445c08f6f0aceb8067009b9699399be8ed6308
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-01-24 22:25:09 +0500

    change

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,13 +1,7 @@
-// const register =async (req ,res)=>{
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// }
+
 const register = async (req ,res)=>{
     try {
-        // const {}
+     
     } catch (error) {
         
         sendResponse(res,500 , null, true , "somethings went wrong" )
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,21 +4,23 @@ import "dotenv/config";
 import morgan from "morgan";
 import cors from "cors";
 import { config } from "./configs/config.js";
-// import {  } from "module";
+import cookieParser from 'cookie-parser';
 const app = express();
 
 // middleware 
 app.use(express.json());
 app.use(cors("*"));
 app.use(morgan("tiny"));
+app.use(cookieParser());
 
 // routes
 app.get("/" , async (req , res)=> res.send("api respose....."));
+// app.use("/api/auth" )
 // process.on()
 
 connectDb().then((data)=>{
     console.log('data >>>',data);
-app.listen(config.Port , ()=> {
+app.listen(config.port , ()=> {
     console.log("server is runnig");
 })
 }).catch(()=>{
