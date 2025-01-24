994fd282a5822738f5533962971c5a1acaa3d70b
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-01-24 22:58:50 +0500

    seting middleware in index file

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
 const port = 5000;
 
+// middlewares
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cookieParser());
+app.use(cors("*"));
+
+// routes
 app.get('/', (req, res) => res.send('Hello World'));
 
 app.listen(port, () => {
